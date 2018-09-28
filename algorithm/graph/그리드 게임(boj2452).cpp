cc63a2c56c905e3f77d767618e9242ca15eef960
dajinstory <dajinstory@naver.com>
2018-09-28 14:07:50 +0900

    grit game conquer

--- algorithm/graph/그리드 게임(boj2452).cpp
+++ algorithm/graph/그리드 게임(boj2452).cpp
@@ -1,44 +1,16 @@
 #include <stdio.h>
 #include <algorithm>
 #include <vector>
-#include <unordered_set>
-//#include <queue>
+#include <queue>
+#define MAX(A,B) ((A)>(B)? (A):(B))
 #define MIN(A,B) ((A)<(B)? (A):(B))
 #define MAX_N 104
-#define QSIZE 1000004
 using namespace std;
 
-class QUEUE{
-public:
-	void push(pair<int, int> t){
-		arr[end] = make_pair(t.first, t.second);
-		end = (end + 1) % QSIZE;
-	}
-	void pop(void){
-		start = (start + 1) % QSIZE;
-	}
-	int empty(void){
-		if (end - start == 0){
-			return 1;
-		}
-		else{
-			return 0;
-		}
-	}
-	pair<int, int> front(void){
-		return arr[start];
-	}
-private:
-	pair<int, int> arr[QSIZE];
-	int start;
-	int end;
-};
-
-//queue<pair<int, int> > myQ;
-QUEUE myQ;
+queue<pair<int, int> > myQ;
 int map[MAX_N][MAX_N];
 vector<int> link[MAX_N * MAX_N];
-unordered_set<int> set[MAX_N * MAX_N];
+vector<int> bpgraph[2];
 int visit[MAX_N * MAX_N];
 int check[MAX_N * MAX_N];
 int xx[4] = { 1, 0, -1, 0 };
@@ -46,7 +18,6 @@ int yy[4] = { 0, 1, 0, -1 };
 int N, M, num;
 
 void bfs_mapping(void);
-pair<int, int> bfs_tree_level(int start);
 int bfs_checking(int start, int ans);
 int main(){
 	scanf("%d%d", &N, &M);
@@ -60,17 +31,55 @@ int main(){
 	bfs_mapping();
 
 	int answer = 987654321;
-	for (int i = 1; i <= num; i++){
-		if (!check[i]){
-			int re = bfs_checking(i, answer);
-			answer = MIN(answer, re);
-			check[i] = 1;
-		}
+	int fst = 0, snd = 1;
+	if (bpgraph[0].size() < bpgraph[1].size()){
+		fst = 1;
+		snd = 0;
 	}
 
+	for (int i = 0; i < bpgraph[fst].size(); i++){
+		int now = bpgraph[fst][i];
+		if (!check[now]){
+			if (link[now].size() == 1){
+				check[now] = 1;
+				continue;
+			}
+			int re = bfs_checking(now, answer);
+			check[now] = re;
+			if (re < answer){
+				bpgraph[snd].clear();
+				answer = re;
+				bpgraph[snd].push_back(now);
+			}
+			else if (re == answer){
+				bpgraph[snd].push_back(now);
+			}
+		}
+	}
+	for (int i = 0; i < bpgraph[snd].size(); i++){
+		int pre = bpgraph[snd][i];
+		for (int j = 0; j < link[pre].size(); j++){
+			int now = link[pre][j];
+			if (!check[now]){
+				if (link[now].size() == 1){
+					check[now] = 1;
+					continue;
+				}
+				int re = bfs_checking(now, answer);
+				check[now] = re;
+				if (re < answer){
+					answer = re;
+				}
+			}
+		}
+	}
+	if (answer == 987654321){
+		answer = num - 1;
+	}
 	printf("%d", answer);
 	return 0;
 }
+
 void bfs_mapping(){
 	int color;
 	for (int i = 1; i <= N; i++){
@@ -78,15 +87,15 @@ void bfs_mapping(){
 			if (map[i][j] < 0){
 				num++;
 				color = map[i][j];
+				bpgraph[color + 2].push_back(num);
 
 				//set zero field for checking overlapped color
-				//for (int k = 0; k < MAX_N * MAX_N; k++){
-				//	visit[k] = 0;
-				//}
+				for (int k = 0; k < MAX_N * MAX_N; k++){
+					visit[k] = 0;
+				}
 
 				map[i][j] = num;
-				//visit[num] = 1;
-				set[num].insert(int(num));
+				visit[num] = 1;
 				myQ.push(make_pair(i, j));
 				while (!myQ.empty()){
 					int x = myQ.front().first;
@@ -99,11 +108,8 @@ void bfs_mapping(){
 							map[nx][ny] = num;
 							myQ.push(make_pair(nx, ny));
 						}
-						else if (map[nx][ny]>0){// && !visit[map[nx][ny]]){
-							auto itr = set[num].find(map[nx][ny]);
-							if (itr != set[num].end())continue;
-							//visit[map[nx][ny]] = 1;
-							set[num].insert(int(map[nx][ny]));
+						else if (map[nx][ny]>0 && !visit[map[nx][ny]]){
+							visit[map[nx][ny]] = 1;
 							link[num].push_back(map[nx][ny]);
 							link[map[nx][ny]].push_back(num);
 						}
@@ -115,8 +121,10 @@ void bfs_mapping(){
 }
 
 int bfs_checking(int start, int ans){
-	int now;
-	int dist;
+	int now, dist;
+	//visit[i] = i번째 노드까지 가는데 얼마만큼 걸리나. 
+	//전체개수가 num이므로 최대치는 num-1, 떄문에, num으로 초기화
+	//ans보다 큰 visit[i]값을 갖는 i는 후보에서 제거
 	for (int i = 1; i <= num; i++){
 		visit[i] = num;
 	}
@@ -148,3 +156,4 @@ int bfs_checking(int start, int ans){
 	}
 	return dist;
 }
+ 
